362e4fa416e673f4945fc3939674fcc4a108b208
Solomon Shalom Lijo <71135230+solomonshalom@users.noreply.github.com>
2023-10-02 17:36:12 +0530

    Updated script.js
    Here's what's changed and improved:
    
    1. Instead of using getElementsByClassName, which returns a collection, I used querySelector to directly select the first matching element with the class name "ytp-ad-skip-button." This makes the code cleaner and easier to understand.
    
    2. I simplified the condition for checking if skipButton is defined. In JavaScript, null, undefined, and 0 are considered "falsy" values, so you don't need to check skipButton !== undefined and skipButton.length > 0. The if (skipButton) condition is sufficient to check if skipButton exists and is not null or undefined.
    
    These changes make the code more concise and easier to maintain while achieving the same functionality.

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-// Check for ads and click the skip button if available
-function checkForAds() {
-    const skipButton = document.querySelector(".ytp-ad-skip-button");
+setInterval(function() {
+    var skipButton = document.querySelector(".ytp-ad-skip-button");
     if (skipButton) {
-        console.log("Ad Detected. Skipping...");
+        console.log("Ad Detected");
         skipButton.click();
     }
-}
-
-// Run the ad-checking function every second (1000 milliseconds)
-setInterval(checkForAds, 1000);
+}, 3000);
